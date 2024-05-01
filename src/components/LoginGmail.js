de5f90fb3622cc714445924ab91adc15d169082a
Vishwas Gaur <vishwas@plus91labs.com>
2024-05-01 13:15:01 +0530

    code push

--- src/components/LoginGmail.js
+++ src/components/LoginGmail.js
@@ -1,17 +1,41 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react'
 
 const LoginGmail = () => {
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-lg max-w-sm w-full text-center">
-        <h2 className="text-2xl font-bold mb-4">Sign in with Google</h2>
-        <button
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          onClick={() => {
-            
-          }}
-        >
-          {/* <svg
+
+    const openPopup = () => {
+        signInWithPopup(authUser, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                console.error("catch error", error)
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-lg max-w-sm w-full text-center">
+                <h2 className="text-2xl font-bold mb-4">Sign in with Google</h2>
+                <button
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    onClick={openPopup()}
+                >
+                    {/* <svg
             className="w-6 h-6 inline-block mr-2"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -31,11 +55,11 @@ const LoginGmail = () => {
               d="M11 21V12l-5.375-3.938L11 4v7h9c1.1 0 2 .9 2 2s-.9 2-2 2h-9"
             />
           </svg> */}
-          Sign In
-        </button>
-      </div>
-    </div>
-  )
+                    Sign In
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default LoginGmail
